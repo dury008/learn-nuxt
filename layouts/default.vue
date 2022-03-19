6bfa2b6c4d57f5a3bd4f0520201d9fb4ebe54280
dury008 <dury008@naver.com>
2022-03-19 22:10:08 +0900

    nuxt study

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,14 @@
 <template>
-    <div>
-        <h2>{{$route.name}}페이지</h2>
-        <NuxtLink to="/">인덱스 페이지</NuxtLink>
-        <NuxtLink to="/main">메인 페이지</NuxtLink>
-        <NuxtLink to="/product">상품 페이지</NuxtLink>
-        
-        <Nuxt/>
-    </div>
+  <div>
+    <header>
+      <h1><NuxtLink to="/">Nuxt Shopping</NuxtLink></h1>
+    </header>
+    <Nuxt />
+  </div>
 </template>
 
 <script>
-    export default {}
+export default {};
 </script>
 
 <style></style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,16 +1,12 @@
 <template>
-  <div>
-      <h1>에러 페이지</h1>
-      <p>페이지를 찾을 수 없습니다.</p>
-  </div>
+    <div>
+        <h1>에러 페이지</h1>
+        <p>페이지를 찾을 수 없습니다.</p>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
